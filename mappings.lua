ecf6f1f3d3fcf59146e42ed0b9c49efea1c3d185
milomilo88 <punkeltje@gmail.com>
2023-06-27 09:59:47 +0900

    small changes

--- mappings.lua
+++ mappings.lua
@@ -19,7 +19,9 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<S-Tab>"] = { ":bprev<cr>", desc = "goto previsou tab"},
+    ["<Tab>"] = { ":bnext<cr>", desc = "goto next tab"},
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,11 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
